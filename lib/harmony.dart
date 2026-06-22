fb269a0f4c5439fc0dfbee9e6afa076ab72d82f8
g4h3n <g4h3nreal@gmail.com>
2026-06-22 14:16:25 -0300

    minigame structure

---
+++ lib/harmony.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Harmony extends StatefulWidget {
+  const Harmony({super.key});
+
+  @override
+  State<Harmony> createState() => _HarmonyState();
+}
+
+class _HarmonyState extends State<Harmony> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/melody.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Melody extends StatefulWidget {
+  const Melody({super.key});
+
+  @override
+  State<Melody> createState() => _MelodyState();
+}
+
+class _MelodyState extends State<Melody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
